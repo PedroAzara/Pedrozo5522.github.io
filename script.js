bd8d2576515a90084e7db7c9ada717ed883e3558
Pedrozo5522 <46581272+Pedrozo5522@users.noreply.github.com>
2020-02-04 21:19:57 -0300

    Interface pronta, agora só falta as dicas e os sistemas de levels

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function gerarNumeroAleatorio(){
-    return Math.floor(Math.random() * 10)
-}
 
 function verificarSeEQuadradoPerfeito(n){
   raiz = Math.sqrt(n)
@@ -25,29 +22,52 @@ function verificarseEParOuImpar(n){
 
 function notificacaoAcerto(){
   console.log('Parabéns, vc acertou')
-    divModal = document.getElementById('modal1')
-    divModal.classList.add('is-active')
-    divModal.classList.add('is-clipped  ')
+    divCaixinha = document.getElementById('caixinha1')
+    divCaixinha.classList.remove('is-danger')
+    divCaixinha.classList.add('is-success')
+    divMsg = document.getElementById('msg1')
+    divMsg.innerHTML = 'Parabéns!<br>Você <strong>acertou!</strong>'
     
-    setTimeout(desaparecer, 1500)
+    
+
+}
+
+function notificacaoErro(){
+  console.log('Desculpe, você errou :(')
+    divCaixinha = document.getElementById('caixinha1')
+    divCaixinha.classList.add('is-danger')
+    divMsg = document.getElementById('msg1')
+    divMsg.innerHTML = 'Desculpe<br>Você errou, tente novamente :('
+    
+    
+    
 }
 
 var desaparecer = function desaparecerNotificacao(){
-  divModal.classList.add('is-active')
+  divCaixinha.classList.remove('is-success')
+  divCaixinha.classList.remove('is-danger')
+  divMsg.innerHTML = 'Digite um número de 1 a 100 e clique em "Confirmar!" para ver seu resultado!'
+  numeroQERA = document.getElementById('numeroEra')
+  numeroQERA.innerHTML = `Resultado`
 }
 
 function escolherNumero(){
   function gerarNumeroAleatorio(){
     return Math.floor(Math.random() * 100)}
+  var numeroRandom = gerarNumeroAleatorio()
   numeroJogadorFormatado = Number.parseInt(document.getElementById('numeroJogador').value)
   if(numeroJogadorFormatado >= 0 && numeroJogadorFormatado <=100){
     console.log(numeroJogadorFormatado)
-    if(numeroJogadorFormatado === 1){
-      notificacaoAcerto()
-    }else{
-      console.log('Vc errou')
-    }
     document.getElementById('aviso').classList.add('invisible')
+    numeroQERA = document.getElementById('numeroEra')
+    numeroQERA.innerHTML = `O número era ${numeroRandom}`
+    if(numeroJogadorFormatado === numeroRandom){
+      notificacaoAcerto()   
+    }else{
+      notificacaoErro()
+    }
+    setTimeout(desaparecer, 8000)
+    
   }else{
     document.getElementById('aviso').classList.remove('invisible')
   }
